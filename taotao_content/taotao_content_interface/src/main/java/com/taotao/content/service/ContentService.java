a5a449d1e94286ffeeae2c18795b2e94e663e952
jyq <542373960@qq.com>
2017-11-13 23:20:38 +0800

    第八次提交项目,前台轮播图添加ok (第五天00:50)

--- taotao_content/taotao_content_interface/src/main/java/com/taotao/content/service/ContentService.java
+++ taotao_content/taotao_content_interface/src/main/java/com/taotao/content/service/ContentService.java
@@ -3,6 +3,9 @@ package com.taotao.content.service;
 import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.pojo.TbContent;
 
+import java.util.List;
+
 public interface ContentService {
     TaotaoResult addContent(TbContent tbContent );
+    List<TbContent> getContentList(long cid);
 }
--- taotao_content/taotao_content_service/src/main/java/com/taotao/content/service/ContentServiceImpl.java
+++ taotao_content/taotao_content_service/src/main/java/com/taotao/content/service/ContentServiceImpl.java
@@ -3,10 +3,12 @@ package com.taotao.content.service;
 import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
+import com.taotao.pojo.TbContentExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class ContentServiceImpl implements ContentService{
@@ -21,5 +23,17 @@ public class ContentServiceImpl implements ContentService{
         tbContent.setUpdated(new Date());
         tbContentMapper.insert(tbContent);
         return TaotaoResult.ok();
+    }
+
+    @Override
+    public List<TbContent> getContentList(long cid) {
+        //根据分类id查询内容列表
+        TbContentExample example = new TbContentExample();
+        //设置查询条件
+        TbContentExample.Criteria criteria = example.createCriteria();
+        criteria.andCategoryIdEqualTo(cid);
+        //执行查询
+        List<TbContent> list = tbContentMapper.selectByExample(example);
+        return list;
     }
 }
