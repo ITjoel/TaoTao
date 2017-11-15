280c85469786987e0d3a62a787f62165b06bb3ef
jyq <542373960@qq.com>
2017-11-15 23:14:01 +0800

    第十次提交项目,商品数据同步到solr. (第六天00:00)

---
+++ taotao_search/taotao_search_service/src/test/java/com/taotao/solrj/TestSolrJ.java
@@ -0,0 +1,127 @@
+package com.taotao.solrj;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.SolrInputDocument;
+import org.junit.Test;
+
+public class TestSolrJ {
+
+	@Test
+	public void testSolrJAddDocument() throws Exception{ 
+		//创建一个SolrServer对象
+		SolrServer solrServer = new HttpSolrServer("http://192.168.113.130:9999/solr");
+		//创建一个文档对象SolrInputDocument
+		SolrInputDocument document = new SolrInputDocument();
+		//向文件中添加域
+		document.addField("id", "1");
+		document.addField("item_title", "测试商品");
+		document.addField("item_price", 100);
+		//把文档写入索引库
+		solrServer.add(document);
+		//提交
+		solrServer.commit();
+	}
+
+	@Test
+	public void deleteDocumentById() throws Exception {
+		// 第一步：创建一个SolrServer对象。
+		SolrServer solrServer = new HttpSolrServer("http://192.168.113.130:9999/solr");
+		// 第二步：调用SolrServer对象的根据id删除的方法。
+		solrServer.deleteById("1");
+		// 第三步：提交。
+		solrServer.commit();
+	}
+
+	@Test
+	public void deleteDocumentByQuery() throws Exception {
+		SolrServer solrServer = new HttpSolrServer("http://192.168.113.130:9999/solr");
+		solrServer.deleteByQuery("title:change.me");
+		solrServer.commit();
+	}
+	
+	@Test
+	public void testQueryIndex() throws Exception {
+		SolrServer solrServer = new HttpSolrServer("http://192.168.131.130:9999/solr");
+		//创建一个Query对象
+		SolrQuery query = new SolrQuery();
+		query.setQuery("三星");
+		query.setStart(0);
+		query.setRows(10);
+		query.set("df", "item_title");
+		query.setHighlight(true);
+		query.addHighlightField("item_title");
+		query.setHighlightSimplePre("<em>");
+		query.setHighlightSimplePost("</em>");
+		//执行查询
+		QueryResponse response = solrServer.query(query);
+		//取查询结果
+		SolrDocumentList solrDocumentList = response.getResults();
+		//查询结果总记录数
+		System.out.println("查询结果总记录数：" + solrDocumentList.getNumFound());
+		for (SolrDocument solrDocument : solrDocumentList) {
+			System.out.println(solrDocument.get("id"));
+			String itemName = null;
+			//取高亮显示
+			Map<String, Map<String, List<String>>> highlighting = response.getHighlighting();
+			List<String> list = highlighting.get(solrDocument.get("id")).get("item_title");
+			if (list != null && list.size() > 0) {
+				itemName = list.get(0);
+			} else {
+				itemName = (String) solrDocument.get("item_title");
+			}
+			System.out.println(itemName);
+			System.out.println(solrDocument.get("item_sell_point"));
+			System.out.println(solrDocument.get("item_price"));
+			System.out.println(solrDocument.get("item_image"));
+			System.out.println(solrDocument.get("item_category_name"));
+			
+		}
+		
+	}
+
+	@Test
+	public void queryDocumentWithHighLighting() throws Exception {
+		// 第一步：创建一个SolrServer对象
+		SolrServer solrServer = new HttpSolrServer("http://192.168.25.154:8080/solr");
+		// 第二步：创建一个SolrQuery对象。
+		SolrQuery query = new SolrQuery();
+		// 第三步：向SolrQuery中添加查询条件、过滤条件。。。
+		query.setQuery("测试");
+		//指定默认搜索域
+		query.set("df", "item_keywords");
+		//开启高亮显示
+		query.setHighlight(true);
+		//高亮显示的域
+		query.addHighlightField("item_title");
+		query.setHighlightSimplePre("<em>");
+		query.setHighlightSimplePost("</em>");
+		// 第四步：执行查询。得到一个Response对象。
+		QueryResponse response = solrServer.query(query);
+		// 第五步：取查询结果。
+		SolrDocumentList solrDocumentList = response.getResults();
+		System.out.println("查询结果的总记录数：" + solrDocumentList.getNumFound());
+		// 第六步：遍历结果并打印。
+		for (SolrDocument solrDocument : solrDocumentList) {
+			System.out.println(solrDocument.get("id"));
+			//取高亮显示
+			Map<String, Map<String, List<String>>> highlighting = response.getHighlighting();
+			List<String> list = highlighting.get(solrDocument.get("id")).get("item_title");
+			String itemTitle = null;
+			if (list != null && list.size() > 0) {
+				itemTitle = list.get(0);
+			} else {
+				itemTitle = (String) solrDocument.get("item_title");
+			}
+			System.out.println(itemTitle);
+			System.out.println(solrDocument.get("item_price"));
+		}
+	}
+}
